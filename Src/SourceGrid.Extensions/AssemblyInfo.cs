0cdfa2ff1c98fbe18a5416054117b3af9141ae19
Mehmet Ulukaya <mehmet.ulukaya@dedas.com.tr>
2024-12-03 13:53:22 +0300

    versioning

--- Src/SourceGrid.Extensions/AssemblyInfo.cs
+++ Src/SourceGrid.Extensions/AssemblyInfo.cs
@@ -1,5 +1,2 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SourceGrid.Extensions")]
-[assembly: AssemblyDescription("Open Source C# Grid Control Extensions")]
